bdd12f0b7b93b032f60aa695bc9392c2836a1529
jeason353 <1398869507@qq.com>
2019-12-22 22:38:30 +0800

    model converges

--- train.py
+++ train.py
@@ -4,33 +4,33 @@ import torch.optim as optim
 import numpy as np
 from pynvml import *
 import os
+from tensorboardX import SummaryWriter
+import time
 
 from dataset.dataset import dataset
 from models.net import net
 from loss import loss
-os.environ['CUDA_VISIBLE_DEVICES']='2'
+os.environ['CUDA_VISIBLE_DEVICES']='0'
 
-# nvmlInit()
-# handle = nvmlDeviceGetHandleByIndex(0)
-# info = nvmlDeviceGetMemoryInfo(handle)
-
-def compute_accuracy(net, test_loader):
-    net.eval()
+def compute_accuracy(model, test_loader):
+    model.eval()
     with torch.no_grad():
         accuracys = []
         for i, (inputs, labels) in enumerate(test_loader):
             labels = labels.float()
             if torch.cuda.is_available():
                 inputs, labels = inputs.cuda(), labels.cuda()
-            predicted = net(inputs)
+            predicted = model(inputs)
 
             for i in range(3):
+
                 predicted[i][predicted[i] > 0.5] = 1
                 predicted[i][predicted[i] <= 0.5] = 0
             predicted = predicted[0] + predicted[1] + predicted[2]
-            predicted = torch.sum(predicted, dim=1)
-            predicted[predicted >= 2] = 1
+            
             predicted[predicted < 2] = 0
+            predicted[predicted >= 2] = 1
+            predicted = predicted.view(-1)
             
             accuracy = torch.sum(predicted == labels).item() / labels.size()[0]
             accuracys.append(accuracy)
@@ -58,49 +58,43 @@ if cuda:
 criterion = loss()
 optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
+writer = SummaryWriter(log_dir='scalar')
+
 if cuda:
     criterion = criterion.cuda()
+iter_n = 0
+t = time.strftime("%m-%d-%H-%M", time.localtime())
 for epoch in range(1, EPOCHS + 1):
     for i, (inputs, labels) in enumerate(train_loader):
         torch.cuda.empty_cache()
-        # modelsize(model, inputs)
-        # inputs, labels = data
-        # print(i, inputs.size())
+
         optimizer.zero_grad()
-        
-        # label = torch.tensor(labels)
-        # print(inputs.dtype, type(labels))
 
         labels = labels.float()
         if cuda:
             inputs, labels = inputs.cuda(), labels.cuda()
-        
-        # print("Memory Total:{}, Free:{}, Used:{}".format(info.total, info.free, info.used))
-        # print(labels.size())
-        # with torch.no_grad():
-        predicted = model(inputs)
-        if torch.max(predicted[0]).item() > 1 or torch.min(predicted[0]).item()<0:
-            print(torch.max(predicted[0]).item())
-        # print(predicted[0].size())
 
-        loss = criterion(*predicted, labels)
+        predicted = model(inputs)
+
+        loss = criterion(*predicted, labels)  
+        
         loss.backward()
         optimizer.step()
 
         accuracy = compute_accuracy(model, test_loader)
 
+        writer.add_scalar(t+'/train_loss', loss.item(), iter_n)
+        writer.add_scalar(t+'/test_accuracy', accuracy, iter_n)
+
+        iter_n += 1
+
+        if i == 26:
+            torch.save(model.state_dict(), 'model.pth')
+
         print('accuracy:{}'.format(accuracy))
 
-        # if epoch < 2:
-        #     lr = lr * 2
-        #     for param_group in optimizer.param_groups:
-        #         param_group['lr'] = lr
-        # else:
-        #     lr = lr * 0.1
-        #     for param_group in optimizer.param_groups:
-        #         param_group['lr'] = lr
 
-        if i % 1 == 0:
+        if i % 10 == 0:
             print('Epoch[{}/{}], iter {}, loss: {}'.format(epoch, EPOCHS, i, loss.item()))
-        # print("Memory Total:{}, Free:{}, Used:{}".format(info.total, info.free, info.used))
 
+writer.close()
