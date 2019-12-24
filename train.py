2d35e17e256ed13feeb2970f61581b0e9a7be34f
jeason353 <1398869507@qq.com>
2019-12-24 17:50:09 +0800

    converge on train dataset

--- train.py
+++ train.py
@@ -10,36 +10,23 @@ import time
 from dataset.dataset import dataset
 from models.net import net
 from loss import loss
-os.environ['CUDA_VISIBLE_DEVICES']='0'
+os.environ['CUDA_VISIBLE_DEVICES']='1'
 
-def compute_accuracy(model, test_loader):
-    model.eval()
-    with torch.no_grad():
-        accuracys = []
-        for i, (inputs, labels) in enumerate(test_loader):
-            labels = labels.float()
-            if torch.cuda.is_available():
-                inputs, labels = inputs.cuda(), labels.cuda()
-            predicted = model(inputs)
-
-            for i in range(3):
-
-                predicted[i][predicted[i] > 0.5] = 1
-                predicted[i][predicted[i] <= 0.5] = 0
-            predicted = predicted[0] + predicted[1] + predicted[2]
-            
-            predicted[predicted < 2] = 0
-            predicted[predicted >= 2] = 1
-            predicted = predicted.view(-1)
-            
-            accuracy = torch.sum(predicted == labels).item() / labels.size()[0]
-            accuracys.append(accuracy)
-        
-        return sum(accuracys) / len(accuracys)
+def compute_accuracy(predicted, labels):
+    for i in range(3):
+        predicted[i][predicted[i] > 0.5] = 1
+        predicted[i][predicted[i] <= 0.5] = 0
+    predicted = predicted[0] + predicted[1] + predicted[2]
+    
+    predicted[predicted < 2] = 0
+    predicted[predicted >= 2] = 1
+    predicted = predicted.view(-1)
+    accuracy = torch.sum(predicted == labels).item() / labels.size()[0]
+    return accuracy
 
 
 BATCH_SIZE = 32
-EPOCHS = 10
+EPOCHS = 1
 LEARNING_RATE = 0.001
 
 np.random.seed(0)
@@ -47,16 +34,17 @@ torch.manual_seed(1)
 
 cuda = torch.cuda.is_available()
 
-train_set = dataset(train=False)
+train_set = dataset(train=True)
 test_set = dataset(train=False)
 train_loader = torch.utils.data.DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True)
-test_loader = torch.utils.data.DataLoader(test_set, batch_size=BATCH_SIZE, shuffle=False)
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=2*BATCH_SIZE, shuffle=False)
 
 model = net()
 if cuda:
     model = model.cuda()
 criterion = loss()
 optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+# optimizer = optim.SGD(model.parameters(), lr=LEARNING_RATE)
 
 writer = SummaryWriter(log_dir='scalar')
 
@@ -64,6 +52,7 @@ if cuda:
     criterion = criterion.cuda()
 iter_n = 0
 t = time.strftime("%m-%d-%H-%M", time.localtime())
+print(len(train_loader))
 for epoch in range(1, EPOCHS + 1):
     for i, (inputs, labels) in enumerate(train_loader):
         torch.cuda.empty_cache()
@@ -81,20 +70,30 @@ for epoch in range(1, EPOCHS + 1):
         loss.backward()
         optimizer.step()
 
-        accuracy = compute_accuracy(model, test_loader)
+        accuracy = compute_accuracy(predicted, labels)
 
         writer.add_scalar(t+'/train_loss', loss.item(), iter_n)
-        writer.add_scalar(t+'/test_accuracy', accuracy, iter_n)
+        writer.add_scalar(t+'/train_accuracy', accuracy, iter_n)
+        
+        if i % 100 == 0:
+            with torch.no_grad():
+                accuracys = []
+                for i_, (inputs_, labels_) in enumerate(test_loader):
+                    labels_ = labels_.float()
+                    if cuda:
+                        inputs_, labels_ = inputs_.cuda(), labels_.cuda()
+                    predicted_ = model(inputs_)
+                    accuracys.append(compute_accuracy(predicted_, labels_))
+                accuracy_ = sum(accuracys) / len(accuracys)
+                writer.add_scalar(t+'/test_accuracy', accuracy_, iter_n)
+            print('test loss:{:.6f}'.format(accuracy_))
 
         iter_n += 1
 
-        if i == 26:
+        if i == 500:
             torch.save(model.state_dict(), 'model.pth')
 
-        print('accuracy:{}'.format(accuracy))
-
-
         if i % 10 == 0:
-            print('Epoch[{}/{}], iter {}, loss: {}'.format(epoch, EPOCHS, i, loss.item()))
+            print('Epoch[{}/{}], iter {}, loss:{:.6f}, accuracy:{}'.format(epoch, EPOCHS, i, loss.item(), accuracy))
 
 writer.close()
