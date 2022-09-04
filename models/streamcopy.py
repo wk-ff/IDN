b74ec3505531df4722ecc9b50b71543a4963d576
fuji37450 <40747030s@ntnu.edu.tw>
2022-09-04 14:41:45 +0800

    refactor(stream.py): undone

--- models/streamcopy.py
+++ models/streamcopy.py
@@ -6,33 +6,22 @@ import cv2
 
 class stream(nn.Module):
     def __init__(self):
+        def block(in_f, out_f):
+            return nn.Sequential(
+                nn.Conv2d(in_f, out_f, 3, stride=1, padding=1),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(out_f, out_f, 3, stride=1, padding=1),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(2, stride=2)
+            )
         super(stream, self).__init__()
 
-        self.stream = nn.Sequential(
-            nn.Conv2d(1, 32, 3, stride=1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(32, 32, 3, stride=1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2, stride=2),
-
-            nn.Conv2d(32, 64, 3, stride=1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(64, 64, 3, stride=1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2, stride=2),
-
-            nn.Conv2d(64, 96, 3, stride=1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(96, 96, 3, stride=1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2, stride=2),
-
-            nn.Conv2d(96, 128, 3, stride=1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(128, 128, 3, stride=1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2, stride=2)
-        )
+        self.blocks = [
+            block(1, 32).to(device),
+            block(2, 64).to(device),
+            block(64, 96).to(device),
+            block(96, 128).to(device)
+        ]
 
         self.Conv_32 = nn.Conv2d(32, 32, 3, stride=1, padding=1)
         self.Conv_64 = nn.Conv2d(64, 64, 3, stride=1, padding=1)
@@ -48,17 +37,17 @@ class stream(nn.Module):
 
     def forward(self, reference, inverse):
         for i in range(4):
-            reference = self.stream[0 + i * 5](reference)
-            reference = self.stream[1 + i * 5](reference)
-            inverse = self.stream[0 + i * 5](inverse)
-            inverse = self.stream[1 + i * 5](inverse)
-            inverse = self.stream[2 + i * 5](inverse)
-            inverse = self.stream[3 + i * 5](inverse)
-            inverse = self.stream[4 + i * 5](inverse)
+            reference = self.blocks[i][0](reference)
+            reference = self.blocks[i][1](reference)
+            inverse = self.blocks[i][0](inverse)
+            inverse = self.blocks[i][1](inverse)
+            inverse = self.blocks[i][2](inverse)
+            inverse = self.blocks[i][3](inverse)
+            inverse = self.blocks[i][4](inverse)
             reference = self.attention(inverse, reference)
-            reference = self.stream[2 + i * 5](reference)
-            reference = self.stream[3 + i * 5](reference)
-            reference = self.stream[4 + i * 5](reference)
+            reference = self.blocks[i][2](reference)
+            reference = self.blocks[i][3](reference)
+            reference = self.blocks[i][4](reference)
 
         return reference, inverse
 
@@ -68,9 +57,11 @@ class stream(nn.Module):
 
         up_sample = nn.functional.interpolate(
             inverse, (discrimnative.size()[2], discrimnative.size()[3]), mode='nearest')
+
         conv = getattr(self, 'Conv_' + str(up_sample.size()[1]), 'None')
         g = conv(up_sample)
         g = sigmoid(g)
+
         tmp = g * discrimnative + discrimnative
         f = GAP(tmp)
         f = f.view(f.size()[0], 1, f.size()[1])
@@ -79,13 +70,17 @@ class stream(nn.Module):
         f = fc(f)
         f = sigmoid(f)
         f = f.view(-1, f.size()[2], 1, 1)
+
         out = tmp * f
 
         return out
 
 
 if __name__ == '__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(device)
     model = stream()
     # r, i = model(torch.ones(1,3,32,32), torch.ones(1,3,32,32))
     # print(r.size(), i.size())
-    x = {}
+    img = cv2.imread('dataset/BHSig260/Bengali_56x250/001/B-S-001-G-01.tif')
+    print(model.blocks[0][0](torch.FloatTensor(img).to(device)))
--- utils.py
+++ utils.py
@@ -29,3 +29,61 @@ def plot_roc_curve(fper, tper, filename):
     plt.legend()
     plt.savefig(filename)
     plt.show()
+
+
+def visualize_single_stream(model_dir, refer_dir, test_dir):
+
+    # get model
+    model = net().to(device)
+    model.load_state_dict(torch.load(model_dir))
+
+    # get image
+    refer_img = cv2.imread(refer_dir, 0)
+    test_img = cv2.imread(test_dir, 0)
+    refer_img = refer_img.reshape(-1, refer_img.shape[0], refer_img.shape[1])
+    test_img = test_img.reshape(-1, test_img.shape[0], test_img.shape[1])
+    refer_test = torch.FloatTensor(np.concatenate(
+        (refer_img, test_img), axis=0)).to(device)
+
+    # get Conv2d layers
+    no_of_layers = 0
+    conv_layers = []
+    for child in list(model.stream.stream.children()):
+        if type(child) == nn.Conv2d:
+            no_of_layers += 1
+            conv_layers.append(child)
+        elif type(child) == nn.Sequential:
+            for layer in child.children():
+                if type(layer) == nn.Conv2d:
+                    no_of_layers += 1
+                    conv_layers.append(layer)
+    print(no_of_layers)
+
+    # put image into stream con
+    results = [conv_layers[0](refer_test)]
+    for i in range(1, len(conv_layers)):
+        results.append(conv_layers[i](results[-1]))
+    outputs = results
+
+    # visualize
+    for num_layer in range(len(outputs)):
+        plt.figure(figsize=(50, 10))
+        layer_viz = outputs[num_layer][0, :, :, :]
+        layer_viz = layer_viz.data
+        print("Layer ", num_layer + 1)
+        for i, filter in enumerate(layer_viz):
+            if i == 16:
+                break
+            plt.subplot(2, 8, i + 1)
+            plt.imshow(filter)
+            plt.axis("off")
+        plt.show()
+        plt.close()
+
+
+# if __name__ == '__main__':
+    # model_dir = 'BHSigH_model_90.103161%.pth'
+    # image_root = 'dataset/BHSig260/Bengali_56x250/001/'
+    # refer_dir = f'{image_root}B-S-001-G-01.tif'
+    # test_dir = f'{image_root}B-S-001-F-01.tif'
+    # visualize_single_stream(model_dir, refer_dir, test_dir)
