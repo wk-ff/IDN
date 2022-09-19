34697cde5080588c5c82c4c608bcf76332dc7e39
fuji37450 <40747030s@ntnu.edu.tw>
2022-09-19 14:46:28 +0800

    feat(utils.py): plot frr far curve

--- utils.py
+++ utils.py
@@ -1,27 +1,15 @@
 import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-
-from torch.utils.data import DataLoader
-from torchvision import models
-
-import torchvision.transforms as transforms
-import torchvision.datasets as dataset
 
 import matplotlib.pyplot as plt
 import numpy as np
-import cv2
 
-from models.net import net
-from models.stream import stream
-
+from shapely.geometry import LineString
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-def plot_roc_curve(fper, tper, filename):
-    plt.plot(fper, tper, color='red', label='ROC')
+def plot_roc_curve(fpr, tpr, filename):
+    plt.plot(fpr, tpr, color='red', label='ROC')
     plt.plot([0, 1], [0, 1], color='green', linestyle='--')
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
@@ -29,3 +17,21 @@ def plot_roc_curve(fper, tper, filename):
     plt.legend()
     plt.savefig(filename)
     plt.show()
+
+
+def plot_far_frr_curve(fpr, fnr, threshold):
+    _, ax = plt.subplots()
+
+    ax.plot(threshold, fpr, 'r--', label='FAR')
+    ax.plot(threshold, fnr, 'g--', label='FRR')
+    plt.xlabel('Threshold')
+    plt.xticks([])
+    plt.ylabel('far/frr')
+
+    eer_idx = np.nanargmin(np.absolute((fnr - fpr)))
+    plt.plot(threshold[eer_idx], fpr[eer_idx], 'bo', label='EER')
+
+    ax.legend(loc='upper right', shadow=True, fontsize='x-large')
+
+    plt.title(f'EER: {fpr[eer_idx]:.2%}')
+    plt.savefig('curve.jpg')
