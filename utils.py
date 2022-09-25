4bd2536e332928672072d098efcca6a4bcebfc32
fuji37450 <40747030s@ntnu.edu.tw>
2022-09-25 15:15:07 +0800

    feat(utils.py): draw failed predict samples

--- utils.py
+++ utils.py
@@ -1,9 +1,8 @@
+import os
 import torch
-
+from datetime import datetime
 import matplotlib.pyplot as plt
 import numpy as np
-
-from shapely.geometry import LineString
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -15,7 +14,7 @@ def plot_roc_curve(fpr, tpr, filename):
     plt.ylabel('True Positive Rate')
     plt.title('Receiver Operating Characteristic Curve')
     plt.legend()
-    plt.savefig(filename)
+    plt.savefig(os.path.join('imgs', 'plot', filename))
     plt.show()
 
 
@@ -34,4 +33,37 @@ def plot_far_frr_curve(fpr, fnr, threshold):
     ax.legend(loc='upper right', shadow=True, fontsize='x-large')
 
     plt.title(f'EER: {fpr[eer_idx]:.2%}')
-    plt.savefig('curve.jpg')
+    plt.savefig(os.path.join('imgs', 'plot', 'curve.jpg'))
+
+
+def draw_failed_sample(samples):
+    rows = 1
+    columns = 2
+    
+    folder_name = os.path.join('imgs', 'failed_sample', datetime.now().strftime('%m%d_%H%M%S'))
+    os.mkdir(folder_name)
+    os.mkdir(os.path.join(folder_name, 'fp'))
+    os.mkdir(os.path.join(folder_name, 'fn'))
+    
+    for i, (input, label) in enumerate(samples):
+        reference = input[0]
+        test = input[1]
+        label = int(label)
+
+        fig = plt.figure(figsize=(8, 3))
+        plt.suptitle(f'Ground Truth: {label} Predict: {int(not label)}')
+        
+        fig.add_subplot(rows, columns, 1)
+        plt.imshow(reference, cmap='gray')
+        plt.xticks([])
+        plt.yticks([])
+        plt.title('Reference')
+
+        fig.add_subplot(rows, columns, 2)
+        plt.imshow(test, cmap='gray')
+        plt.xticks([])
+        plt.yticks([])
+        plt.title('Test')
+
+        plt.savefig(os.path.join(folder_name, 'fn' if label == 1 else 'fp', f'index_{i}.jpg'))
+        plt.close()
