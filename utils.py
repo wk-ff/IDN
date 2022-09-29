c0d0343b01eb4c63ff7ac788441df4cfb4725b43
fuji37450 <40747030s@ntnu.edu.tw>
2022-09-29 13:50:36 +0800

    feat(utils.py): visualize feature map of each block in stream

--- utils.py
+++ utils.py
@@ -3,6 +3,8 @@ import torch
 from datetime import datetime
 import matplotlib.pyplot as plt
 import numpy as np
+from models.net import net
+from dataset.dataset import SignatureLoader
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -39,12 +41,13 @@ def plot_far_frr_curve(fpr, fnr, threshold):
 def draw_failed_sample(samples):
     rows = 1
     columns = 2
-    
-    folder_name = os.path.join('imgs', 'failed_sample', datetime.now().strftime('%m%d_%H%M%S'))
+
+    folder_name = os.path.join(
+        'imgs', 'failed_sample', datetime.now().strftime('%m%d_%H%M%S'))
     os.mkdir(folder_name)
     os.mkdir(os.path.join(folder_name, 'fp'))
     os.mkdir(os.path.join(folder_name, 'fn'))
-    
+
     for i, (input, label) in enumerate(samples):
         reference = input[0]
         test = input[1]
@@ -52,7 +55,7 @@ def draw_failed_sample(samples):
 
         fig = plt.figure(figsize=(8, 3))
         plt.suptitle(f'Ground Truth: {label} Predict: {int(not label)}')
-        
+
         fig.add_subplot(rows, columns, 1)
         plt.imshow(reference, cmap='gray')
         plt.xticks([])
@@ -65,63 +68,41 @@ def draw_failed_sample(samples):
         plt.yticks([])
         plt.title('Test')
 
-        plt.savefig(os.path.join(folder_name, 'fn' if label == 1 else 'fp', f'index_{i}.jpg'))
+        plt.savefig(os.path.join(folder_name, 'fn' if label ==
+                    1 else 'fp', f'index_{i}.jpg'))
         plt.close()
 
 
-def visualize_single_stream(model_dir, refer_dir, test_dir):
-
+def visualize_stream(model_dir, dataset_dir, filename, data_idx):
     # get model
     model = net().to(device)
     model.load_state_dict(torch.load(model_dir))
 
-    # get image
-    refer_img = cv2.imread(refer_dir, 0)
-    test_img = cv2.imread(test_dir, 0)
-    refer_img = refer_img.reshape(-1, refer_img.shape[0], refer_img.shape[1])
-    test_img = test_img.reshape(-1, test_img.shape[0], test_img.shape[1])
-    refer_test = torch.FloatTensor(np.concatenate(
-        (refer_img, test_img), axis=0)).to(device)
+    # use forward hooks to get activation map
+    # https://discuss.pytorch.org/t/visualize-feature-map/29597/2
+    activation = {}
 
-    # get Conv2d layers
-    no_of_layers = 0
-    conv_layers = []
-    for child in list(model.stream.stream.children()):
-        if type(child) == nn.Conv2d:
-            no_of_layers += 1
-            conv_layers.append(child)
-        elif type(child) == nn.Sequential:
-            for layer in child.children():
-                if type(layer) == nn.Conv2d:
-                    no_of_layers += 1
-                    conv_layers.append(layer)
-    print(no_of_layers)
+    def get_activation(name):
+        def hook(model, input, output):
+            activation[name] = output.detach().cpu().numpy()
+        return hook
 
-    # put image into stream con
-    results = [conv_layers[0](refer_test)]
-    for i in range(1, len(conv_layers)):
-        results.append(conv_layers[i](results[-1]))
-    outputs = results
+    for i in range(4):
+        model.stream.stream[4 + i * 5] \
+                    .register_forward_hook(get_activation(f'block{i + 1}'))
 
-    # visualize
-    for num_layer in range(len(outputs)):
-        plt.figure(figsize=(50, 10))
-        layer_viz = outputs[num_layer][0, :, :, :]
-        layer_viz = layer_viz.data
-        print("Layer ", num_layer + 1)
-        for i, filter in enumerate(layer_viz):
-            if i == 16:
-                break
-            plt.subplot(2, 8, i + 1)
-            plt.imshow(filter)
-            plt.axis("off")
-        plt.show()
-        plt.close()
+    # get data
+    dataset = SignatureLoader(root=dataset_dir, train=False)
+    data, _ = dataset[data_idx]
+    data = data.to(device).unsqueeze_(0)
+    _ = model(data)
 
-
-# if __name__ == '__main__':
-    # model_dir = 'BHSigH_model_90.103161%.pth'
-    # image_root = 'dataset/BHSig260/Bengali_56x250/001/'
-    # refer_dir = f'{image_root}B-S-001-G-01.tif'
-    # test_dir = f'{image_root}B-S-001-F-01.tif'
-    # visualize_single_stream(model_dir, refer_dir, test_dir)
+    # plot 4 blocks' feature map
+    fig, ax = plt.subplots(4)
+    for i in range(4):
+        act = activation[f'block{i + 1}'].squeeze()
+        ax[i].imshow(np.mean(act, axis=0))
+        ax[i].set_title(f'block{i + 1}')
+        ax[i].axis('off')
+    plt.tight_layout()
+    plt.savefig(os.path.join('imgs', 'vis', filename), dpi=600)
